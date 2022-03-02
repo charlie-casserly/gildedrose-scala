682cf3b54c2ee568a1ee701c178a93e47ad8c4bb
Charlie Casserly <72042975+charlie-casserly@users.noreply.github.com>
2022-03-02 10:39:30 +0000

    Update GildedRose.scala

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -3,20 +3,21 @@ package com.gildedrose
 class GildedRose(val items: Array[Item]) {
 
   def updateQuality() {
-    for (i <- 0 until items.length) qualityChecker(items(i))
+    for (i <- 0 until items.length) {
+      val matchedItem = matchItem(items(i))
+      matchedItem.tick(items(i))
+    }
   }
 
-  private def qualityChecker(item: Item) {
+  private def matchItem(item: Item): Items = {
 
-    val item_instance = item.name match {
+    item.name match {
       case "Normal" => new Normal
       case "Aged Brie" => new Brie
       case "Backstage passes to a TAFKAL80ETC concert" => new BackstagePass
       case "Conjured" => new Conjured
       case "Sulfuras, Hand of Ragnaros" => new Items
     }
-
-    item_instance.tick(item)
 
   }
 
